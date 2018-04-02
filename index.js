5b60f198a571b457419af2c1cef546ee1a4f7c2e
Ariel Davis <ariel.z.davis@icloud.com>
2018-04-02 16:06:12 -0400

    move vijay around

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ function randColor() {
     return "hsl(" + String(Math.random() * 360) + ",100%,50%)"
 }
 
+function randOffset() {
+    return String(Math.random() * 4 - 2) + "px"
+}
+
 function changeVijay() {
     vijay.style.color = randColor()
+    vijay.style.top = randOffset()
+    vijay.style.left = randOffset()
 }
 
 changeVijay()
