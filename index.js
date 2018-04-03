95500024a066d357d6c6499137f599a91a731ba4
vrama628 <vrama628@gmail.com>
2018-04-02 23:19:25 -0400

    swimming

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ function randColor() {
     return "hsl(" + String(Math.random() * 360) + ",100%,50%)"
 }
 
+var x = {a : 0, b : 0, c : false}
 function randOffset() {
-    return String(Math.random() * 4 - 2) + "px"
+    x.c = !x.c
+    if (x.c)
+      x.a = x.a + Math.random() * 4 - 2;
+    else
+      x.b = x.b + Math.random() * 4 - 2
+
+    return String(x.c ? x.a : x.b) + "px"
 }
 
 function changeVijay() {
